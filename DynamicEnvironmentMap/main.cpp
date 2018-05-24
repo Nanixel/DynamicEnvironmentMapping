ae63b5652d04588af82cb0302dd3e91f44275617
Jesse Tellez <jessetellez@jesses-macbook-pro-2.local>
2018-05-24 09:08:05 -0600

    Initial Commit

---
+++ DynamicEnvironmentMap/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  DynamicEnvironmentMap
+//
+//  Created by Jesse Tellez on 5/24/18.
+//  Copyright © 2018 Jesse. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
